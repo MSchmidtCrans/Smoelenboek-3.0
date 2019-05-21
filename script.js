8ffb66a4a00cf8df2f742ce5399b3bc241a2a17f
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-21 11:05:01 +0200

    Debugged filter hide functionality

--- script.js
+++ script.js
@@ -3,52 +3,23 @@
 
 $(document).ready(function(){
 
-  //Call a php script to collect data from backend
-  function indexPull() {
-  $.get("http://10.1.254.102/Smoelenboek-3.0/smoelDataPull.php", function(data, status){
-
-  //Iterate through the JSON array for all entries
-  for (x in data) {
-
-   //Set variables
-    let indexCardClass = "indexKaart";
-    let strFirstLtr = data[x].lastName.charAt(0);
-    
-    //Check for class values and set accordingly
-    data[x].gender == "man" ? indexCardClass += " man": indexCardClass += " vrouw";
-    if (strFirstLtr.match(/[a-i]/i)){indexCardClass += " aToti"};
-    if (strFirstLtr.match(/[j-r]/i)){indexCardClass += " jTotr"};
-    if (strFirstLtr.match(/[s-z]/i)){indexCardClass += " sTotz"};
-   
-    //Create new div and insert into DOM
-    $("#cards").append('<div class="' + indexCardClass + '"><div class="persInfo">' + '<p>Naam: ' + data[x].firstName
-                        + ' ' + data[x].lastName + '</br>Woonplaats: ' + data[x].city + ' </br>Geslacht: ' + data[x].gender + '</p></div></div>');
-      }
-   });
-   return;
-  }
-
   //Call data pull function
   indexPull();
+  
 
    /*script for hiding/showing indexcards
     Cards will only appear after a filter is applied
     A alpabatical filter needs to be applied before
     you can filter based on gender*/
 
-    //Hide all cards until a filter is applied
-    $(".indexKaart").hide();
-    $(".genderIndex").hide();
-
-    
+    //Hide filter buttons until a filter is applied
+    cloak(".genderIndex");  
 
     //Trigger event handler
     $("button").click(function(){
 
         //Hide all cards
-        $(".indexKaart").hide();
-
-
+        cloak(".indexKaart");
 
         //Make or unmake buttoncolor red (=active) upon click event
         if($(this).css("background-color") == "rgb(255, 0, 0)") {
@@ -82,7 +53,7 @@ $(document).ready(function(){
 
          //Only show the genderfilter when a alpabatical filter has been selected
          if (x < 1) {
-            $(".genderIndex").hide();
+            cloak(".genderIndex");
               } else {
             $(".genderIndex").show();
          }
@@ -99,4 +70,35 @@ $(document).ready(function(){
         if (reslijst != null)
           reslijst.show();
     });
+
+  //Call a php script to collect data from backend
+  function indexPull() {
+   $.get("http://10.1.254.102/Smoelenboek-3.0/smoelDataPull.php", function(data, status){
+ 
+   //Iterate through the JSON array for all entries
+   for (x in data) {
+ 
+    //Set variables
+     let indexCardClass = "indexKaart";
+     let strFirstLtr = data[x].lastName.charAt(0);
+     
+     //Check for class values and set accordingly
+     data[x].gender == "man" ? indexCardClass += " man": indexCardClass += " vrouw";
+     if (strFirstLtr.match(/[a-i]/i)){indexCardClass += " aToti"};
+     if (strFirstLtr.match(/[j-r]/i)){indexCardClass += " jTotr"};
+     if (strFirstLtr.match(/[s-z]/i)){indexCardClass += " sTotz"};
+    
+     //Create new div and insert into DOM
+     $("#cards").append('<div class="' + indexCardClass + '"><div class="persInfo">' + '<p>Naam: ' + data[x].firstName
+                         + ' ' + data[x].lastName + '</br>Woonplaats: ' + data[x].city + ' </br>Geslacht: ' + data[x].gender + '</p></div></div>');
+      
+      //Hide cards until filter is applied
+      cloak(".indexKaart");   
+                        }
+    });
+   }
+
+   function cloak(val) {
+      $(val).hide();
+   }
 });
